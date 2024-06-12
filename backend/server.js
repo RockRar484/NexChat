a84c394083c2467d1b7bc029726c30db1b08a558
RockRar484 <czileen484patel@gmail.com>
2024-06-13 03:26:28 +0530

    User backend done and tested successfully

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT;
 
