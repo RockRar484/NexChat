5a7b2e62dc22c36ef67daeca3fb83140522f6111
RockRar484 <czileen484patel@gmail.com>
2024-06-13 03:14:59 +0530

    Database setup done

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
