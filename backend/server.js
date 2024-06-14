66e349ad90f87fe5719e330ef54d5a241279b481
RockRar484 <czileen484patel@gmail.com>
2024-06-15 01:21:33 +0530

    Message model created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
 dotenv.config();
@@ -11,7 +13,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT;
 
 const server = app.listen(
